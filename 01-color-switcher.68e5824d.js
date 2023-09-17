5a99299446f6a414ede5d0ece7f111ef77198d04
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2023-09-17 12:33:38 +0000

    Deploying to gh-pages from @ Kaliuzhnyi-Nazariy/goit-js-hw-09@544cb94c89e07030225005cab7ba61144c661eb9 🚀

---
+++ 01-color-switcher.68e5824d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;t.addEventListener("click",(function(){e=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0})),n.addEventListener("click",(function(){clearInterval(e),t.disabled=0}))}();
+//# sourceMappingURL=01-color-switcher.68e5824d.js.map
